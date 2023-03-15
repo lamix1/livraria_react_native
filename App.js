aed5c8c50f30df65f34c4ab26210d158effd832e
lamix1 <camilacristina328santoss@gmail.com>
2023-03-15 11:12:11 -0300

    commit

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import Header from './src/components/Header.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Livraria</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
